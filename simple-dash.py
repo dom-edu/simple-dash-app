e95b8270a6e53f91d2f39c60e69089f1ade5ab84
dom-edu <84454639+dom-edu@users.noreply.github.com>
2025-12-16 16:52:01 -0500

    Implemented our simple apple stock example, updated requirements

--- simple-dash.py
+++ simple-dash.py
@@ -1,10 +1,33 @@
-from dash import Dash, html
+from dash import Dash, html, dcc # core components
+import pandas as pd 
+import plotly.express as px
+
+# import plotly lineplot
+
+AAPL_URL = "https://raw.githubusercontent.com/matplotlib/sample_data/refs/heads/master/aapl.csv"
+
+# load the data in via pandas 
+aapl_df = pd.read_csv(AAPL_URL)
+
+# convert Date to Date time 
+aapl_df['Date'] = pd.to_datetime(aapl_df['Date'])
+
+# sort the values by Date
+aapl_df.sort_values(by="Date", inplace=True) # 1984 - 2007 
+
+# make figures
+line_plot = px.line(aapl_df, x="Date", y="Close", title='Apple Stock Closing Price (USD) 1984 - 2008')
 
 # instantiating the app 
 app = Dash()
 
 # adding elements to the app
-app.layout = [html.Div(children='Hello World')]
+app.layout = [
+
+    html.H1(children='Apple Stock App', style={'textAlign':'center'}), 
+    dcc.Graph(id='graph-content', figure=line_plot)
+
+]
 
 if __name__ == '__main__':
     app.run(debug=True)
